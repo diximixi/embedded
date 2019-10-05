4bb986483206dde45e69f5c6f73c84b0f71c4841
Michael Hadorn <michaelhadorn@Users-iMac.local>
2019-10-05 19:06:51 +0200

    corrections

--- tools/copyToAdafruit.py
+++ tools/copyToAdafruit.py
@@ -7,9 +7,9 @@ import yesno
 
 workpath = os.getcwd()
 print ("Files to copy")
-for path, _, files in os.walk(path):
+for path, _, files in os.walk(workpath):
     for name in files:
-        print os.path.join(workpath, name)
+        print (" "+os.path.join(os.path.relpath(path), name))
 
 okay = yesno.query_yes_no("Copy?", default="no")
 if (okay):
