76e977be57c412d2b5b03ef643d7a6bdeae07906
Michael Hadorn <michaelhadorn@Users-iMac.local>
2019-10-05 18:43:09 +0200

    new copyToAdafruit.py and yesno.py

--- tools/copyToAdafruit.py
+++ tools/copyToAdafruit.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 """copyToAdafruit.py copies all .py files found in the current directory to the Adafruit Trinket drive /Volumes/CIRCUITPY"""
 
-import sys
+import os
+import shutil
+import yesno
 
+workpath = os.getcwd()
+print ("Files to copy")
+for path, _, files in os.walk(path):
+    for name in files:
+        print os.path.join(workpath, name)
+
+okay = yesno.query_yes_no("Copy?", default="no")
+if (okay):
+    shutil.copytree(path, "/Volumes/CIRCUITPY")
